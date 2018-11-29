a7e7e01e7d7b23fa7ee5dce293d79dde523cff65
Sam Locke <samlocke619@gmail.com>
2018-11-28 22:00:27 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,10 +4,13 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.SelectionMode;
@@ -23,8 +26,8 @@ import javafx.scene.text.Text;
 public class Main extends Application {
     Stage window;
     Scene scene;
-    ListView<String> foodListView;
-    ListView<String> mealListView;
+    ListView<HBox> foodListView2;
+    ListView<HBox> mealListView2;
     
     public VBox makeLeftPane() {
         VBox leftPaneOuter = new VBox(2);
@@ -72,40 +75,224 @@ public class Main extends Application {
             window = primaryStage;
             window.setTitle("Food Query and Meal Analysis");
 
-            foodListView = new ListView<>();
-            foodListView.getItems().addAll("Banana", "Burger", "Lamb's Eye", "Pizza", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q");
-            foodListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+            foodListView2 = new ListView<HBox>();
 
-            mealListView = new ListView<>();
-            mealListView.getItems().addAll("Burger");
-            mealListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-
-            /*Button button1 = new Button("Button Number 1");
-            Button button2 = new Button("Button Number 2");
-            Button button3 = new Button("Button Number 3");
-
-            FlowPane flowPane = new FlowPane();
-
-            flowPane.setOrientation(Orientation.VERTICAL);
-            flowPane.setVgap(10);
-
-            flowPane.getChildren().add(button1);
-            flowPane.getChildren().add(button2);
-            flowPane.getChildren().add(button3);*/
-
+            HBox hbox;
+            VBox vbox;
+            CheckBox cb1;
+            Label foodName = new Label();
+            Label calories = new Label();
+            Label fat = new Label();
+            Label carbs = new Label();
+            Label fiber = new Label();
+            Label protein = new Label();
+            Label id = new Label();
+            
+            for(int i = 0; i < 8; ++i) {
+                hbox = new HBox();
+                hbox.setPadding(new Insets(10, 10, 10, 10));
+                
+                vbox = new VBox();
+                vbox.setPadding(new Insets(10, 10, 10, 10));
+                
+                cb1 = new CheckBox();
+                cb1.setText("Add to Meal");
+                
+                vbox.getChildren().add(cb1);
+                
+                switch(i) {
+                    case 0 : 
+                        foodName = new Label("Name: Burger");
+                        calories = new Label("Calories: 1000");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000000");
+                        break;
+                    case 1 : 
+                        foodName = new Label("Name: Soda");
+                        calories = new Label("Calories: 100");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000001");
+                        break;
+                    case 2 : 
+                        foodName = new Label("Name: Fries");
+                        calories = new Label("Calories: 2000");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000002");
+                        break;
+                    case 3 : 
+                        foodName = new Label("Name: Cigarette");
+                        calories = new Label("Calories: -80");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000003");
+                        break;
+                        
+                    case 4 : 
+                        foodName = new Label("Name: Banana");
+                        calories = new Label("Calories: 2");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000004");
+                        break;
+                    case 5 : 
+                        foodName = new Label("Name: Tomato");
+                        calories = new Label("Calories: 7");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000005");
+                        break;
+                    case 6 : 
+                        foodName = new Label("Name: Steak");
+                        calories = new Label("Calories: 2000");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000006");
+                        break;
+                    case 7 : 
+                        foodName = new Label("Name: Rubber Ball");
+                        calories = new Label("Calories: 0");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000007");
+                        break;
+                }
+                
+                vbox.getChildren().addAll(foodName, calories, fat, carbs, fiber, protein, id);
+                
+                hbox.getChildren().addAll(vbox, cb1);
+                
+                foodListView2.getItems().add(hbox);
+            }
+            
+            mealListView2 = new ListView<HBox>();
+            
+            for(int i = 1; i < 3; ++i) {
+                hbox = new HBox();
+                hbox.setPadding(new Insets(10, 10, 10, 10));
+                
+                vbox = new VBox();
+                vbox.setPadding(new Insets(10, 10, 10, 10));
+                
+                cb1 = new CheckBox();
+                cb1.setText("Remove From Meal");
+                
+                vbox.getChildren().add(cb1);
+                
+                switch(i) {
+                    case 0 : 
+                        foodName = new Label("Name: Burger");
+                        calories = new Label("Calories: 1000");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000000");
+                        break;
+                    case 1 : 
+                        foodName = new Label("Name: Soda");
+                        calories = new Label("Calories: 100");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000001");
+                        break;
+                    case 2 : 
+                        foodName = new Label("Name: Fries");
+                        calories = new Label("Calories: 2000");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000002");
+                        break;
+                    case 3 : 
+                        foodName = new Label("Name: Cigarette");
+                        calories = new Label("Calories: -80");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000003");
+                        break;
+                        
+                    case 4 : 
+                        foodName = new Label("Name: Banana");
+                        calories = new Label("Calories: 2");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000004");
+                        break;
+                    case 5 : 
+                        foodName = new Label("Name: Tomato");
+                        calories = new Label("Calories: 7");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000005");
+                        break;
+                    case 6 : 
+                        foodName = new Label("Name: Steak");
+                        calories = new Label("Calories: 2000");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000006");
+                        break;
+                    case 7 : 
+                        foodName = new Label("Name: Rubber Ball");
+                        calories = new Label("Calories: 0");
+                        fat = new Label("Fat: 5g");
+                        carbs = new Label("Carbs: 5g");
+                        fiber = new Label("Fiber: 5g");
+                        protein = new Label("Protein: 5g");
+                        id = new Label("ID: 000000007");
+                        break;
+                }
+                
+                vbox.getChildren().addAll(foodName, calories, fat, carbs, fiber, protein, id);
+                
+                hbox.getChildren().addAll(vbox, cb1);
+                
+                mealListView2.getItems().add(hbox);
+            }
 
             GridPane root = new GridPane();
 
-            root.setPadding(new Insets(20));
-            root.setHgap(25);
-            root.setVgap(15);
+            root.setPadding(new Insets(10, 10, 10, 10));
+            root.setHgap(10);
+            root.setVgap(10);
 
             Label labelTitle = new Label("Button Stuff");
             Label foodListTitle = new Label("Food List");
             Label mealListTitle = new Label("Meal List");
 
-            Button addToMealButt = new Button("Add To Meal");
+            Button addToMealButt = new Button("Add Selected To Meal");
             Button analyzeMealButt = new Button("Analyze Meal");
+            Button removeFromMealButt = new Button("Remove Selected From Meal");
 
             root.add(labelTitle, 0, 0);
 
@@ -113,14 +300,15 @@ public class Main extends Application {
             root.add(leftBar, 0, 1);
 
             root.add(foodListTitle, 1, 0, 1, 1);
-            root.add(foodListView, 1, 1, 2, 1);
+            root.add(foodListView2, 1, 1, 2, 1);
             root.add(addToMealButt, 2, 0);
 
             root.add(mealListTitle, 3, 0, 1, 1);
-            root.add(mealListView, 3, 1, 2, 1);
-            root.add(analyzeMealButt, 4, 0);
+            root.add(mealListView2, 3, 1, 3, 1);
+            root.add(analyzeMealButt, 4, 0, 1, 1);
+            root.add(removeFromMealButt, 5, 0);
 
-            scene = new Scene(root, 1000, 500);
+            scene = new Scene(root, 800, 500);
             window.setScene(scene);
             window.show();  
             
