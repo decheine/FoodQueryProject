cad0a044f1f5c0b1b70fd700ddd6c465b243f373
Sam Locke <samlocke619@gmail.com>
2018-12-12 01:05:29 -0600

    Update SaveFile.java

--- SaveFile.java
+++ SaveFile.java
@@ -1,8 +1,25 @@
+/**
+ * Filename:   SaveFile.java
+ * Project:    Final Project
+ * Authors:    Nick Decheine, Samuel Locke
+ *
+ * Semester:   Fall 2018
+ * Course:     CS400
+ * Lecture:    002
+ * 
+ * Due Date:   Before 10pm on December 12, 2018
+ * Version:    1.0
+ * 
+ * Credits:    None
+ * 
+ * Bugs:       None
+ */
+
 package application;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,25 +28,23 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 
 /**
- * Creates a new window for allowing the input of a new food item's attributes.
+ * Creates a new window for saving a list of food items to a given file name
  * 
- * @author Nick Decheine
- *
+ * @author Nick Decheine, Samuel Locke
  */
 public class SaveFile {
-    final Stage dialog; // stage for this window
-    VBox windowBox; // contains all the input fields
+    private final Stage dialog; // stage for this window
+    private VBox windowBox; // contains all the input fields
     
     /**
-     * Constructor
+     * Public constructor
      * 
      * @param primaryStage - the primary stage the program is running on
      */
-    public SaveFile(Stage primaryStage, FoodData foodData) {
+    public SaveFile(Stage primaryStage, FoodDataADT<FoodItem> foodData) {
         dialog = new Stage();
         dialog.initModality(Modality.APPLICATION_MODAL);
         dialog.initOwner(primaryStage);
@@ -51,9 +66,6 @@ public class SaveFile {
         
         EventHandler<ActionEvent> saveButtonEvent = new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e) {
-                // here is the action of the save button
-                
-                // save(entryField.getText()+".csv", foodData); // TODO: need to add on?
                 foodData.saveFoodItems(entryField.getText());
                 dialog.close();
             }
